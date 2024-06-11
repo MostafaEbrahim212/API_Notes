59aa163ba4cdf7fec666dc82503c2edca00b899e
MostafaEbrahim212 <sasaebrahim1@yahoo.com>
2024-06-11 17:59:09 +0300

    end the project
    Complete simple CRUD Operation and authentication project

--- app/Http/Controllers/User/NotesController.php
+++ app/Http/Controllers/User/NotesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Http\Resources\NoteResource;
+use App\Models\Note;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
@@ -12,7 +15,25 @@ class NotesController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        $notes = $user->notes;
+        // return every note with notes resource
+        $notes = $notes->map(function ($note) {
+            return new NoteResource($note);
+        });
+        if ($notes->count() > 0) {
+            return res_data(
+                $notes,
+                'notes found',
+                200
+            );
+        } else {
+            return res_data(
+                [],
+                'there is no notes yet',
+                200
+            );
+        }
     }
 
     /**
@@ -20,7 +41,17 @@ class NotesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string'
+        ]);
+        $request['user_id'] = Auth::id();
+        $note = Note::create($request->all());
+        return res_data(
+            new NoteResource($note),
+            'Note created successfully',
+            201
+        );
     }
 
     /**
@@ -28,7 +59,24 @@ class NotesController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = Auth::user();
+        $note = Note::where('id', $id)
+            ->where('user_id', $user->id)
+            ->first();
+
+        if ($note) {
+            return res_data(
+                new NoteResource($note),
+                'note found',
+                200
+            );
+        } else {
+            return res_data(
+                [],
+                'note not found or you do not have access to this note',
+                404
+            );
+        }
     }
 
     /**
@@ -36,7 +84,28 @@ class NotesController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string'
+        ]);
+        $user = Auth::user();
+        $note = Note::where('id', $id)
+            ->where('user_id', $user->id)
+            ->first();
+        if ($note) {
+            $note->update($request->all());
+            return res_data(
+                new NoteResource($note),
+                'note updated successfully',
+                200
+            );
+        } else {
+            return res_data(
+                [],
+                'note not found or you do not have access to this note',
+                404
+            );
+        }
     }
 
     /**
@@ -44,6 +113,23 @@ class NotesController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = Auth::user();
+        $note = Note::where('id', $id)
+            ->where('user_id', $user->id)
+            ->first();
+        if ($note) {
+            $note->delete();
+            return res_data(
+                [],
+                'note deleted successfully',
+                200
+            );
+        } else {
+            return res_data(
+                [],
+                'note not found or you do not have access to this note',
+                404
+            );
+        }
     }
 }
--- routes/customRoute/user.php
+++ routes/customRoute/user.php
@@ -15,5 +15,3 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('profile', [AuthController::class, 'profile']);
     route::apiResource('notes', NotesController::class);
 });
-
-
